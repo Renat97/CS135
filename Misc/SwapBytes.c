04500e1648f763f6347e8d0bc2d6213c61da12eb
Renat97 <renat97@gmail.com>
2020-09-17 08:54:38 -0700

    misc folder added

---
+++ Misc/SwapBytes.c
@@ -0,0 +1,33 @@
+// project to swap the bytes of hexidecimal numbers
+
+#include <stdio.h>
+
+unsigned short swap_bytes(unsigned short i);
+
+int main(void)
+
+{
+
+unsigned short i;
+
+printf("Enter a hexadecimal number (up to four digits): ");
+
+scanf("%hx", &i);
+
+printf("Number with bytes swapped: %hx\n", swap_bytes(i));
+
+return 0;
+
+}
+
+unsigned short swap_bytes(unsigned short i)
+
+{
+
+unsigned short high_byte = i << 8;
+
+unsigned short low_byte = i >> 8;
+
+return high_byte | low_byte;
+
+}
